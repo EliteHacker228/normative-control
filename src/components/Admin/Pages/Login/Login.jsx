8d127a6a16c32f571ca83b24b38fd67c3df2750c
EliteHacker228 <mchappycraft@yandex.ru>
2022-06-13 03:41:29 +0500

    update admin login page design

--- src/components/Admin/Pages/Login/Login.jsx
+++ src/components/Admin/Pages/Login/Login.jsx
@@ -3,11 +3,40 @@ import file_ico from './file-ico.svg';
 import globe_ico from './glode-ico.svg';
 import {NavLink} from "react-router-dom";
 import back_arrow from "../Login/back_arrow.svg";
+import admin_ico from './admin_ico.png';
+import login_ico from './login_ico.png';
+import password_ico from './password_ico.png';
 import React, {Component} from "react";
 import state from "../../../../storage/storage";
 
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        console.log('Состояние в локал сторидже Login');
+        console.log(localStorage.getItem('normokontrol_state'));
+        this.state = JSON.parse(localStorage.getItem('normokontrol_state'));
+        if (this.state === null) {
+            this.state = state;
+        }
+        console.log(this.state);
+    }
+
+    componentDidMount() {
+
+        if (this.state['credentials'] !== undefined && this.state['credentials'] !== 'null') {
+            setTimeout(() => {
+                document.getElementById('reroute').click();
+            }, 0);
+        }else{
+            console.log(this.state['credentials']);
+        }
+    }
+
+    // redirect = () => {
+    //     document.getElementById('reroute').click();
+    // };
 
     login = (evt) => {
         evt.preventDefault();
@@ -33,6 +62,7 @@ class Login extends Component {
                 localStorage.setItem('normokontrol_state', JSON.stringify(state));
                 console.log(result);
                 console.log(state);
+                document.getElementById('reroute').click();
             })
             .catch(error => console.log('error', error));
     };
@@ -41,10 +71,15 @@ class Login extends Component {
         return (
             <div className={css.body}>
                 <form className={css.login_form} onSubmit={this.login} id="login_form">
-                    <input type="text" placeholder="E-mail" name="email"/>
-                    <input type="password" placeholder="Пароль" name="password"/>
+                    <img className={css.admin_ico} src={admin_ico}/>
+                    <p>Вход в панель управления</p>
+                    <img className={css.login_ico} src={login_ico}/>
+                    <input type="text" placeholder="email" name="email"/>
+                    <img className={css.password_ico} src={password_ico}/>
+                    <input type="password" placeholder="password" name="password"/>
                     <input type="submit" value="Войти"/>
                 </form>
+                <NavLink id="reroute" to='/admin_panel/search_file' style={{display: "none"}}/>
             </div>
         );
     }
