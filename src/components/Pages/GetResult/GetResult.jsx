6f435956868f2e5be29cbd68e1268cca749bef81
EliteHacker228 <mchappycraft@yandex.ru>
2022-07-10 21:43:16 +0500

    do some cleanup with eslint

--- src/components/Pages/GetResult/GetResult.jsx
+++ src/components/Pages/GetResult/GetResult.jsx
@@ -4,104 +4,9 @@ import back_arrow from "../GetResult/back_arrow.svg";
 import file_ico from "../GetResult/file_ico.svg";
 import {NavLink} from "react-router-dom";
 import config from "../../../config/config";
+import translations from "../../../translations/translations";
 
 let state;
-
-let translations = {
-    'CHAPTER_ANNOTATION_NOT_FOUND': 'Раздел "Реферат" не найден',
-    'CHAPTER_ANNOTATION_POSITION_MISMATCH': 'Раздел "Реферат": некорректная позиция',
-    'CHAPTER_APPENDIX_NOT_FOUND': 'Раздел "Приложение" не найден',
-    'CHAPTER_APPENDIX_POSITION_MISMATCH': 'Раздел "Приложение": некорректная позиция',
-    'CHAPTER_BODY_DISORDER': 'Основная часть находится на некорректной позиции',
-    'CHAPTER_BODY_NOT_FOUND': 'Раздел "Основная часть" не найден',
-    'CHAPTER_BODY_POSITION_MISMATCH': 'Раздел "Основная часть": некорректная позиция',
-    'CHAPTER_CONCLUSION_NOT_FOUND': 'Раздел "Заключение" не найден',
-    'CHAPTER_CONCLUSION_POSITION_MISMATCH': 'Раздел "Заключение": некорректная позиция',
-    'CHAPTER_CONTENTS_NOT_FOUND': 'Раздел "Содержание" не найден',
-    'CHAPTER_CONTENTS_POSITION_MISMATCH': 'Раздел "Содержание": некорректная позиция',
-    'CHAPTER_EMPTY': 'Пустая часть',
-    'CHAPTER_FRONT_PAGE_NOT_FOUND': 'Раздел "Титульный лист" не найден',
-    'CHAPTER_FRONT_PAGE_POSITION_MISMATCH': 'Раздел "Титульный лист": некорректная позиция',
-    'CHAPTER_INTRODUCTION_NOT_FOUND': 'Раздел "Введение" не найден',
-    'CHAPTER_INTRODUCTION_POSITION_MISMATCH': 'Раздел "Введение": некорректная позиция',
-    'CHAPTER_NO_ONE_CHAPTER_FOUND': 'Не найдено ни одного раздела',
-    'CHAPTER_REFERENCES_NOT_FOUND': 'Раздел "Список литературы" не найден',
-    'CHAPTER_REFERENCES_POSITION_MISMATCH': 'Раздел "Список литературы": некорректная позиция',
-    'CHAPTER_UNDEFINED_CHAPTER': 'Неопознанная часть',
-    'DOCUMENT_UNEXPECTED_CONTENT': 'Неопознанный контент на уровне параграфа',
-    'LIST_LEVEL_MORE_THAN_2': 'Уровень вложенности списка больше 2',
-    'ORDERED_LIST_INCORRECT_MARKER_FORMAT': 'Некорректный формат маркера ненумерованного списка',
-    'ORDERED_LIST_INCORRECT_MARKER_FORMAT_AT_LEVEL_1': 'Некорректный формат маркера первого уровня нумерованного списка',
-    'ORDERED_LIST_INCORRECT_MARKER_FORMAT_AT_LEVEL_2': 'Некорректный формат маркера второго уровня нумерованного списка',
-    'PAGE_HEIGHT_IS_INCORRECT': 'Некорректная ширина страницы',
-    'PAGE_MARGIN_BOTTOM_IS_INCORRECT': 'Некорректный нижний отступ страницы',
-    'PAGE_MARGIN_LEFT_IS_INCORRECT': 'Некорректный левый отступ страницы',
-    'PAGE_MARGIN_RIGHT_IS_INCORRECT': 'Некорректный правый отступ страницы',
-    'PAGE_MARGIN_TOP_IS_INCORRECT': 'Некорректный верхний отступ страницы',
-    'PAGE_WIDTH_IS_INCORRECT': 'Некорректная высота страницы',
-    'PARAGRAPH_UNEXPECTED_CONTENT': 'Неопознанный контент на уровне рана',
-    'PICTURE_IS_NOT_INLINED': 'Изображение не встроено в текст',
-    'PICTURE_REQUIRED_BLANK_LINE_AFTER_PICTURE_TITLE': 'Необходима пустая строка после подписи изображения',
-    'PICTURE_REQUIRED_BLANK_LINE_BEFORE_PICTURE': 'Необходима пустая строка перед изображением',
-    'PICTURE_TITLE_ENDS_WITH_DOT': 'Точка в конце подписи к изображению',
-    'PICTURE_TITLE_NOT_CENTERED': 'Некорректное выравнивание подписи к изображению',
-    'PICTURE_TITLE_NUMBER_DISORDER': 'Неверный порядок нумерации изображений',
-    'PICTURE_TITLE_REQUIRED_LINE_BREAK_BETWEEN_PICTURE_AND_TITLE': 'Необходим перенос строки между изображением и подписью к нему',
-    'PICTURE_TITLE_WRONG_FORMAT': 'Неверный формат подписи изображений',
-    'RUN_UNEXPECTED_CONTENT': 'Неопознанный контент на уровне текста',
-    'TEXT_BODY_SUBHEADER_LEVEL_WAS_MORE_THAN_3': 'Уровень вложенности подразделов больше 3',
-    'TEXT_BODY_SUBHEADER_NUMBER_ORDER_MISMATCH': 'Неверный порядок нумерации подразделов',
-    'TEXT_COMMON_BACKGROUND_FILL': 'Найдена заливка фона у текста',
-    'TEXT_COMMON_BORDER': 'Найдена рамка у параграфа',
-    'TEXT_COMMON_FONT': 'Некорректный шрифт текста',
-    'TEXT_COMMON_HIGHLIGHT': 'Найден текст с выделением',
-    'TEXT_COMMON_INCORRECT_FONT_SIZE': 'Некорректный размер шрифта текста',
-    'TEXT_COMMON_INDENT_FIRST_LINES': 'Некорректный отступ первой строки параграфа',
-    'TEXT_COMMON_INDENT_LEFT': 'Некорректный левый отступ параграфа',
-    'TEXT_COMMON_INDENT_RIGHT': 'Некорректный правый отступ параграфа',
-    'TEXT_COMMON_ITALIC_TEXT': 'Найден курсивный текст',
-    'TEXT_COMMON_RUN_SPACING': 'Некорректный межсимвольный интервал текста',
-    'TEXT_COMMON_STRIKETHROUGH': 'Найден перечеркнутый текст',
-    'TEXT_COMMON_TEXT_COLOR': 'Некорректный цвет текста',
-    'TEXT_COMMON_UNDERLINED': 'Текст подчеркнут',
-    'TEXT_HEADER_ALIGNMENT': 'Некорректное выравнивание заголовка раздела',
-    'TEXT_HEADER_BODY_ALIGNMENT': 'Некорректное выравнивание заголовка/подзаголовка основной части',
-    'TEXT_HEADER_BODY_UPPERCASE': 'Заголовок основной части написан строчными буквами',
-    'TEXT_HEADER_EMPTY': 'Пустой заголовок раздела',
-    'TEXT_HEADER_EMPTY_LINE_AFTER_HEADER_REQUIRED': 'Не найдена пустая строка после заголовка',
-    'TEXT_HEADER_LINE_SPACING': 'Некорректный межстрочный интервал заголовка',
-    'TEXT_HEADER_NOT_BOLD': 'Не полужирный заголовок',
-    'TEXT_HEADER_NOT_UPPERCASE': 'Заголовок раздела написан не строчными буквами',
-    'TEXT_HEADER_REDUNDANT_DOT': 'Точка на конце заголовка',
-    'TEXT_HYPERLINKS_NOT_ALLOWED_HERE': 'Ссылки запрещены в данном разделе',
-    'TEXT_REGULAR_INCORRECT_ALIGNMENT': 'Некорректное выравнивание текста',
-    'TEXT_REGULAR_LINE_SPACING': 'Некорректный межстрочный интервал текста',
-    'TEXT_REGULAR_UPPERCASE': 'Текст написан строчными буквами',
-    'TEXT_REGULAR_WAS_BOLD': 'Полужирный текст',
-    'TEXT_WHITESPACE_AFTER_HEADER_ALIGNMENT': 'Некорректное выравнивание пустой строки после заголовка',
-    'TEXT_WHITESPACE_AFTER_HEADER_BOLD': 'Полужирная пустая строка после заголовка',
-    'TEXT_WHITESPACE_AFTER_HEADER_UPPERCASE': 'Пустая строка после заголовка отформатирована в режиме только строчных букв',
-    'TEXT_WHITESPACE_ALIGNMENT': 'Некорректное выравнивание пустой строки',
-    'TEXT_WHITESPACE_BACKGROUND_FILL': 'Найдена заливка фона у пустой строки',
-    'TEXT_WHITESPACE_BOLD': 'Полужирная пустая строка',
-    'TEXT_WHITESPACE_BORDER': 'Найдена рамка у пустого параграфа',
-    'TEXT_WHITESPACE_FONT': 'Некорректный шрифт пустой строки',
-    'TEXT_WHITESPACE_HIGHLIGHT': 'Найдена пустая строка с выделением',
-    'TEXT_WHITESPACE_INCORRECT_FONT_SIZE': 'Некорректный размер шрифта пустой строки',
-    'TEXT_WHITESPACE_INDENT_FIRST_LINES': 'Некорректный отступ первой строки пустого параграфа',
-    'TEXT_WHITESPACE_INDENT_LEFT': 'Некорректный левый отступ пустого параграфа',
-    'TEXT_WHITESPACE_INDENT_RIGHT': 'Некорректный правый отступ пустого параграфа',
-    'TEXT_WHITESPACE_ITALIC': 'Найден курсивная пустая строка',
-    'TEXT_WHITESPACE_LINE_SPACING': 'Некорректный межстрочный интервал пустой строки',
-    'TEXT_WHITESPACE_RUN_SPACING': 'Некорректный межсимвольный интервал пустой строки',
-    'TEXT_WHITESPACE_STRIKETHROUGH': 'Найдена перечеркнутая пустая строка',
-    'TEXT_WHITESPACE_TEXT_COLOR': 'Некорректный цвет пустой строки',
-    'TEXT_WHITESPACE_UNDERLINED': 'Пустая строка подчеркнута',
-    'TEXT_WHITESPACE_UPPERCASE': 'Пустая строка отформатирована в режиме только строчных букв',
-    'TODO_ERROR': 'Неопределенная ошибка (будет обработана позже)',
-    'WORD_GRAMMATICAL_ERROR': 'Грамматическая ошибка, которую нашел Word',
-    'WORD_SPELL_ERROR': 'Ошибка правописания, которую нашел Word'
-};
 
 const RenderList = (props) => {
     let elements = props.elements;
@@ -119,17 +24,17 @@ const RenderClickableList = (props) => {
     if (props.elements[0].length !== 0) {
         elements = props.elements[0];
     }
-    console.log('props');
-    console.log(typeof props.elements[0]);
-    console.log(props.elements[0]);
     let res = [];
-    for(let i = 0; i < elements.length; i++){
+    for (let i = 0; i < elements.length; i++) {
         res.push(elements[i]);
     }
     return (
         <ul>
             {res.map(element => (
-                <li onClick={props.elements[1]} data-paragraph-id={element['paragraph-id']}>{translations[element['mistake-type']]}</li>
+                <li>
+                    <span onClick={props.elements[1]}
+                          data-paragraph-id={element['paragraph-id']}>{translations[element['mistake-type']]}</span>
+                </li>
             ))}
         </ul>
     );
@@ -141,7 +46,7 @@ class GetResult extends Component {
         super();
         state = JSON.parse(localStorage.getItem('normokontrol_state'));
         state['errors'] = [];
-        state['full_errors'] = {
+        state['fullErrors'] = {
             elements: []
         };
         state['checkResult'] = {};
@@ -159,7 +64,7 @@ class GetResult extends Component {
             .then(result => {
                 state['checkResult'] = JSON.parse(result);
                 let res = state.checkResult.errors;
-                state['full_errors'] = state.checkResult.errors;
+                state['fullErrors'] = state.checkResult.errors;
                 state['errors'] = res.map(x => x['mistake-type']).sort(x => x['paragraph-id']).map(error_code => translations[error_code]);
                 this.forceUpdate();
             })
@@ -173,14 +78,13 @@ class GetResult extends Component {
     componentDidMount() {
         if (!state['errors'].length) {
             this.getResult();
-            console.log(state);
         }
 
         let lastScrollTop = 0;
-        window.addEventListener("scroll", function(){
+        window.addEventListener("scroll", function () {
             let st = document.documentElement.scrollTop;
             let list = document.getElementById('errors_list');
-            if (st > lastScrollTop && list.style.height === '0px' && window.scrollY > 230){ //Скроллим вверх
+            if (st > lastScrollTop && list.style.height === '0px' && window.scrollY > 230) { //Скроллим вверх
                 document.getElementById('toggle_title').click();
             } else if (st < lastScrollTop && list.style.height !== '0px' && window.scrollY < 450) { //Скроллим вниз
                 document.getElementById('toggle_title').click();
@@ -254,33 +158,32 @@ class GetResult extends Component {
         }
     };
 
-    iframeScroll(evt){
+    iframeScroll(evt) {
         let iframe = document.getElementById("test");
         let scrollTargetElement = iframe.contentWindow.document.getElementsByTagName("p")[evt.target.attributes['data-paragraph-id'].nodeValue];
         scrollTargetElement.style.backgroundColor = 'cyan';
-        console.log(scrollTargetElement);
         scrollTargetElement.scrollIntoView();
     };
 
-    getFileHtml(){
+    getFileHtml() {
         let requestOptions = {
             method: 'GET',
             redirect: 'follow'
         };
 
         let iframe = document.getElementById("test");
-        iframe = iframe.contentWindow || ( iframe.contentDocument.document || iframe.contentDocument);
+        iframe = iframe.contentWindow || (iframe.contentDocument.document || iframe.contentDocument);
 
         fetch(`${config['apiAddress']}/document/${state['documentId']}/render?access-key=${state['accessKey']}`, requestOptions)
             .then(response => response.text())
             .then(result => {
-                    iframe.document.open();
-                    iframe.document.write(result);
-                    iframe.document.close();
-            }).then( _ => {
+                iframe.document.open();
+                iframe.document.write(result);
+                iframe.document.close();
+            }).then(_ => {
                 let iframe = document.getElementById("test");
-                let errors = state['full_errors'];
-                for(let i = 0; i < errors.length; i++){
+                let errors = state['fullErrors'];
+                for (let i = 0; i < errors.length; i++) {
                     if (iframe.contentWindow.document.getElementsByTagName("p")[errors[i]['paragraph-id']].title === "") {
                         iframe.contentWindow.document.getElementsByTagName("p")[errors[i]['paragraph-id']].title = translations[errors[i]['mistake-type']];
                     } else {
@@ -290,7 +193,7 @@ class GetResult extends Component {
                     iframe.contentWindow.document.getElementsByTagName("p")[errors[i]['paragraph-id']].style.backgroundColor = 'cyan';
                 }
             }
-            )
+        )
             .catch(error => console.log('error', error));
     };
 
@@ -300,10 +203,10 @@ class GetResult extends Component {
                 <div id="result_box" className={css.result_box}>
                     <div className={css.result_header}>
                         <NavLink className={css.back_arrow_link} to='/upload'>
-                            <img src={back_arrow} title="На главную"/>
+                            <img alt="Стрелка 'Назад'" src={back_arrow} title="На главную"/>
                         </NavLink>
                         <span className={css.file_block}>
-                            <img src={file_ico}/>
+                            <img alt="Значок файла" src={file_ico}/>
                             <span>{state['fileName']}</span>
                         </span>
                     </div>
@@ -326,7 +229,8 @@ class GetResult extends Component {
                 </div>
 
                 <div className={css.toggle_statistics} style={{
-                    display: 'none'}}>
+                    display: 'none'
+                }}>
                     <button id="toggle_button_hide" onClick={this.toggleListDisplay}/>
                     <button id="toggle_button_show" onClick={this.toggleListDisplay}/>
                     <p id="toggle_title" onClick={this.toggleListDisplay}>Развернуть список ошибок</p>
@@ -336,14 +240,14 @@ class GetResult extends Component {
                 </div>
                 <div id="holder" style={{height: '58vh', transition: '0.5s all'}}>
                 </div>
-                <iframe id="downloader" style={{display: 'none'}}/>
+                <iframe title="Ваш документ" id="downloader" style={{display: 'none'}}/>
                 <hr/>
                 <div className={css.test_results}>
                     <div className={css.test_errors_list} id="errors">
-                        <RenderClickableList elements={[state['full_errors'], this.iframeScroll]}/>
+                        <RenderClickableList elements={[state['fullErrors'], this.iframeScroll]}/>
                     </div>
                     <div className={css.test_document_view} id="document">
-                        <iframe title="test" id="test"  name="test"/>
+                        <iframe title="test" id="test" name="test"/>
                     </div>
                 </div>
             </div>
